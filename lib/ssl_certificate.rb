d8d94699edf89294fabc24dde0dccc8cae265550
fujishin05 <your.email@example.com>
2017-09-06 22:55:41 +0900

    Add some methods

--- lib/ssl_certificate.rb
+++ lib/ssl_certificate.rb
@@ -1,5 +1,29 @@
 require "ssl_certificate/version"
+require "openssl"
 
 module SslCertificate
-  # Your code goes here...
+  class Certificate < OpenSSL::X509::Certificate
+    def common_name
+      common_name = subject.to_a.find { |value| value[0] == 'CN' }
+      if common_name
+        common_name[1]
+      else
+        nil
+      end
+    end
+
+    def alternative_name
+      alt_name = extensions.find { |ex| ex.oid == 'subjectAltName' }
+      if alt_name
+        alt_name.value.split(',').map { |dns| dns.strip.gsub(/^DNS:/, '') }.join("\n")
+      else
+        nil
+      end
+    end
+
+    def check_private_key_str(private_key_str)
+      private_key = OpenSSL::PKey::RSA.new(private_key_str)
+      check_private_key(private_key)
+    end
+  end
 end
